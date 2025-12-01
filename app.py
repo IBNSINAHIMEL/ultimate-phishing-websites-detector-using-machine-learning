e876d4e971e29289af879f3376db17b5116614e6
IBN SINA <114084494+IBNSINAHIMEL@users.noreply.github.com>
2025-12-01 11:59:14 +0600

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash
+from flask import Flask, render_template, request, jsonify
 import pickle
 import joblib
 import os
@@ -1515,6 +1515,7 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"❌ Error starting server: {e}")
         app.run(debug=True, host='0.0.0.0', port=5001, use_reloader=False)
+
 
 
 
